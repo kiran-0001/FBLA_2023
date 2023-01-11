bf0463e3963a475195157ec6de765d129547a390
SShashi <shashi.salavath@gmail.com>
2023-01-10 18:49:46 -0600

    v1Points and node config

--- firebase.js
+++ firebase.js
@@ -1,56 +1,72 @@
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyA8zVnO0fL99J99bRxTqcZbmKvdxdYPWDs",
-    authDomain: "fblacap2023.firebaseapp.com",
-    databaseURL: "https://fblacap2023-default-rtdb.firebaseio.com",
-    projectId: "fblacap2023",
-    storageBucket: "fblacap2023.appspot.com",
-    messagingSenderId: "928148561689",
-    appId: "1:928148561689:web:e39e9b05925f81cf78ef88",
-    measurementId: "G-JQYWB5VZ5C"
-  };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8zVnO0fL99J99bRxTqcZbmKvdxdYPWDs",
+  authDomain: "fblacap2023.firebaseapp.com",
+  databaseURL: "https://fblacap2023-default-rtdb.firebaseio.com",
+  projectId: "fblacap2023",
+  storageBucket: "fblacap2023.appspot.com",
+  messagingSenderId: "928148561689",
+  appId: "1:928148561689:web:e39e9b05925f81cf78ef88",
+  measurementId: "G-JQYWB5VZ5C",
+};
 
-  // init firebase
-  firebase.initializeApp(firebaseConfig);
+// init firebase
+firebase.initializeApp(firebaseConfig);
 
-  // reference database
-  var entryFormDB = firebase.database().ref('studentEntries')
+// reference database
+var entryFormDB = firebase.database().ref("studentEntries");
 
-  document.getElementById('totalEntryForm').addEventListener('submit', submitForm)
+document
+  .getElementById("totalEntryForm")
+  .addEventListener("submit", submitForm);
 
-  function submitForm(e) {
-    e.preventDefault();
+function submitForm(e) {
+  e.preventDefault();
 
-    var FirstName = getElementVal('FirstName')
-    var LastName = getElementVal('LastName')
-    var date = getElementVal('date')
-    var grades = getElementVal('grades')
+  var FirstName = getElementVal("FirstName");
+  var LastName = getElementVal("LastName");
+  var date = getElementVal("date");
+  var grades = getElementVal("grades");
+  var points = 0;
 
-    //TODO - add queries for tab selector elements 
-    //var sportingEvent = getElementVal('sportingEvent')
-    //var otherEvent = getElementVal('otherEvent')
+  //TODO - add query for tab selector radio type elements
+  var sportingEvent = document.querySelector('input[name="sportEvent"]:checked').value;
+  var otherEvent = document.querySelector('input[name="otherEvent"]:checked').value;
 
-    //verifiy data fetching
-    //console.log(firstName, lastName, eventDate, studentGrade)
-
-    saveEntry(FirstName, LastName, date, grades);
-
-    //reset form after submission
-    document.getElementById('totalEntryForm').reset(); 
+  if (sportingEvent != "none") {
+    points++;
   }
-
-  //add entry to firebase db
-  const saveEntry = (firstName, lastName, eventDate, studentGrade) => {
-    var newEntry = entryFormDB.push();
-    newEntry.set({
-        firstName : firstName,
-        lastName : lastName,
-        eventDate : eventDate,
-        studentGrade : studentGrade
-    })
+  if (otherEvent != "none") {
+    points++;
   }
+  //verify data fetching
+  console.log(FirstName, LastName, date, grades, sportingEvent, otherEvent, points);
 
-  const getElementVal = (id) => {
-    return document.getElementById(id).value;
-  }
+  saveEntry(FirstName, LastName, date, grades, sportingEvent, otherEvent, points);
+
+  //reset form after submission
+  document.getElementById("totalEntryForm").reset();
+}
+
+//add entry to firebase db
+const saveEntry = (firstName, lastName, eventDate, studentGrade, sportEventTitle, otherEventTitle, pointTotal) => {
+  //config node name in lieu of rand key gen 
+  var entryName = firstName + "_" + lastName;
+  var newEntry = entryFormDB.child(entryName);
+
+  //entry push fields
+  newEntry.set({
+    firstName: firstName,
+    lastName: lastName,
+    eventDate: eventDate,
+    studentGrade: studentGrade,
+    sportEventTitle: sportEventTitle,
+    otherEventTitle: otherEventTitle,
+    pointTotal: pointTotal
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
